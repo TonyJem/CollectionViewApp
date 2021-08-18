ab80772a8097e5320b37dd315377e55a7b8f372d
TonyJem <a.jemanov@gmail.com>
2021-08-18 13:24:18 +0300

    Add imageView constraints

--- CollectionViewApp/CollectionViewApp/ViewController.swift
+++ CollectionViewApp/CollectionViewApp/ViewController.swift
@@ -8,5 +8,12 @@ class ViewController: UIViewController {
         let imageView = UIImageView(image: UIImage(named: "01"))
         
         view.addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 300).isActive = true
     }
 }
