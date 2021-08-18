5b9d51c2edf48a82b0f48ab8ca4a257009a9176f
TonyJem <a.jemanov@gmail.com>
2021-08-18 16:59:43 +0300

    Replace RandomViewController with SwipingController

--- CollectionViewApp/CollectionViewApp/SceneDelegate.swift
+++ CollectionViewApp/CollectionViewApp/SceneDelegate.swift
@@ -7,11 +7,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let randomViewController = UIViewController()
-        randomViewController.view.backgroundColor = .yellow
+        let layout = UICollectionViewLayout()
+        let swipingViewController = SwipingController(collectionViewLayout: layout)
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = randomViewController
+        window.rootViewController = swipingViewController
         window.makeKeyAndVisible()
         self.window = window
     }
