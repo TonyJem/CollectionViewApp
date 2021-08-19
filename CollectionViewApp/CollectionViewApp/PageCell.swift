c4073e5e82f0cb268ab97dc1dd010c823a0e151a
TonyJem <a.jemanov@gmail.com>
2021-08-19 08:03:58 +0300

    Move adding SubViews into  setupLayout func

--- CollectionViewApp/CollectionViewApp/PageCell.swift
+++ CollectionViewApp/CollectionViewApp/PageCell.swift
@@ -33,9 +33,6 @@ class PageCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        addSubview(topImageContainerView)
-        topImageContainerView.addSubview(bearImageView)
-        addSubview(descriptionTextView)
         setupLayout()
     }
     
@@ -44,15 +41,18 @@ class PageCell: UICollectionViewCell {
     }
     
     private func setupLayout() {
+        addSubview(topImageContainerView)
         topImageContainerView.topAnchor.constraint(equalTo: topAnchor).isActive = true
         topImageContainerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         topImageContainerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         topImageContainerView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5).isActive = true
         
+        topImageContainerView.addSubview(bearImageView)
         bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
         bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
         bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
         
+        addSubview(descriptionTextView)
         descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
         descriptionTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24).isActive = true
         descriptionTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24).isActive = true
