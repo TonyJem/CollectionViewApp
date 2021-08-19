ca5e3eaa9c2e52096067b9b0205a9297c90310a8
TonyJem <a.jemanov@gmail.com>
2021-08-19 10:45:07 +0300

    Move UIColor extension to it's own file

---
+++ CollectionViewApp/CollectionViewApp/UIColor+Extension.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+extension UIColor {
+    static var mainPink = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+    static var secondaryPink = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+}
--- CollectionViewApp/CollectionViewApp/ViewController.swift
+++ CollectionViewApp/CollectionViewApp/ViewController.swift
@@ -1,10 +1,5 @@
 import UIKit
 
-extension UIColor {
-    static var mainPink = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
-    static var secondaryPink = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
-}
-
 class ViewController: UIViewController {
     
     let topImageContainerView: UIView = {
